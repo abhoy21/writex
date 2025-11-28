b35b2f7829573f156bc0e4078298b2d199588222
Abhoy Sarkar <sarkar.ab07@gmail.com>
2025-11-28 21:36:56 +0530

    auth config fix

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -180,6 +180,17 @@ export const config = {
     strategy: "jwt",
     maxAge: 24 * 60 * 60,
   },
+  cookies: {
+  sessionToken: {
+    name: `__Secure-next-auth.session-token`,
+    options: {
+      httpOnly: true,
+      sameSite: "lax",
+      path: "/",
+      secure: process.env.NODE_ENV === "production",
+    },
+  },
+},
   secret: process.env.NEXTAUTH_SECRET!,
 } satisfies NextAuthConfig;
 
